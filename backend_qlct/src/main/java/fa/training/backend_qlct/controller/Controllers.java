d1569c439cf2e6803d5d56ec16418ef2adc9c8c7
luannguyen <luantranedu99@gmail.com>
2026-05-09 15:20:28 +0700

    push backend maven project

--- backend_qlct/src/main/java/fa/training/backend_qlct/controller/Controllers.java
+++ backend_qlct/src/main/java/fa/training/backend_qlct/controller/Controllers.java
@@ -1,4 +1,13 @@
 package fa.training.backend_qlct.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controllers {
+    @GetMapping("/hello")
+    public String getString(){
+        return "Hello World";
+    }
+
 }
---
+++ backend_qlct/src/test/java/fa/training/backend_qlct/BackendQlctApplicationTests.java
@@ -0,0 +1,13 @@
+package fa.training.backend_qlct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendQlctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
