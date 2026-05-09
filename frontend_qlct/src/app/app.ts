c99046b43fa916c2e5fb9174fc6197c892636983
luannguyen <luantranedu99@gmail.com>
2026-05-09 13:17:46 +0700

    update fe

--- frontend_qlct/src/app/app.ts
+++ frontend_qlct/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('fronend_qlct');
+  protected readonly title = signal('frontend_qlct');
 }
